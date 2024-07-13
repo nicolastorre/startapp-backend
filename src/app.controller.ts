602128d1466017682058d1df52a718cb32c99a26
merlier <merlier220@gmail.com>
2024-07-13 16:22:11 +0200

    add .env config

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
+    // get an environment variable
+    const dbUser = this.configService.get<string>('DATABASE_USER');
+
+    // get a custom configuration value
+    const dbHost = this.configService.get<string>('database.host');
+
+    console.log(dbUser, dbHost);
+
     return this.appService.getHello();
   }
 }
