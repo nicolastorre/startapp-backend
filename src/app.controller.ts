d4350141a650e2ec571d4d92349c087ecc592741
nicolastorre <10921378+nicolastorre@users.noreply.github.com>
2024-09-14 10:09:05 +0200

    modify custom logs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,24 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ConfigService } from '@nestjs/config';
+import { Public } from './auth/decorators/IsPublic';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private readonly configService: ConfigService,
+    private logger: Logger,
   ) {}
 
-  @Get()
-  getHello(): string {
-    // get an environment variable
-    const dbUser = this.configService.get<string>('DATABASE_USER');
+  @Get('getHealthCheck')
+  @Public()
+  getHealthcheck(): string {
+    Logger.debug('test log getHealthcheck debug');
 
-    // get a custom configuration value
-    const dbHost = this.configService.get<string>('database.host');
-
-    console.log(dbUser, dbHost);
-
-    return this.appService.getHello();
+    return this.appService.getHealthCheck();
   }
 }
--- src/config/winstonConfiguration.ts
+++ src/config/winstonConfiguration.ts
@@ -1,9 +1,17 @@
 import * as path from 'path';
 import * as winston from 'winston';
 
-const logPath = path.join(__dirname, '..', '..', 'logs');
+const logPath = path.join(__dirname, '..', 'logs');
+
+const customFormat = winston.format.printf(({ level, message, timestamp }) => {
+  return `${timestamp} [${level}]: ${message}`;
+});
 
 export const winstonConfig = {
+  format: winston.format.combine(
+    winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    customFormat,
+  ),
   transports: [
     new winston.transports.Console({
       format: winston.format.combine(
@@ -17,7 +25,8 @@ export const winstonConfig = {
       level: 'error',
     }),
     new winston.transports.File({
-      filename: path.join(logPath, 'error.log'),
+      filename: path.join(logPath, 'all.log'),
+      level: 'info',
     }),
   ],
 };
